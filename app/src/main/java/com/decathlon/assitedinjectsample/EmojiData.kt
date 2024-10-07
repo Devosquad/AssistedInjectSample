78e89a0a0acfbe46af689fcbaff5fbfc6a9ae1b2
Renaud Mathieu <renaud.mathieu.partner@decathlon.com>
2024-10-07 21:53:35 +0200

    4️⃣ Setup Details / List for Shared Elements

---
+++ app/src/main/java/com/decathlon/assitedinjectsample/EmojiData.kt
@@ -0,0 +1,39 @@
+package com.decathlon.assitedinjectsample
+
+data class EmojiItem(
+    val emoji: String,
+    val description: String
+)
+
+val emojiItems = listOf(
+    EmojiItem("\uD83D\uDE00", "Grinning Face"),
+    EmojiItem("\uD83D\uDE02", "Face with Tears of Joy"),
+    EmojiItem("\uD83D\uDE04", "Smiling Face with Open Mouth and Smiling Eyes"),
+    EmojiItem("\uD83D\uDE09", "Winking Face"),
+    EmojiItem("\uD83D\uDE0D", "Smiling Face with Heart-Shaped Eyes"),
+    EmojiItem("\uD83D\uDE18", "Face Throwing a Kiss"),
+    EmojiItem("\uD83D\uDE1C", "Face with Stuck-Out Tongue and Winking Eye"),
+    EmojiItem("\uD83D\uDE21", "Pouting Face"),
+    EmojiItem("\uD83D\uDE2D", "Loudly Crying Face"),
+    EmojiItem("\uD83D\uDE31", "Face Screaming in Fear"),
+    EmojiItem("\uD83D\uDE33", "Flushed Face"),
+    EmojiItem("\uD83D\uDE42", "Slightly Smiling Face"),
+    EmojiItem("\uD83D\uDE43", "Upside-Down Face"),
+    EmojiItem("\uD83E\uDD14", "Thinking Face"),
+    EmojiItem("\uD83D\uDC4D", "Thumbs Up Sign"),
+    EmojiItem("\uD83D\uDC4E", "Thumbs Down Sign"),
+    EmojiItem("\uD83D\uDC4C", "OK Hand Sign"),
+    EmojiItem("\uD83D\uDE4F", "Clapping Hands Sign"),
+    EmojiItem("\uD83D\uDC4F\uD83C\uDFFB", "Clapping Hands Sign: Light Skin Tone"),
+    EmojiItem("\uD83D\uDC80", "Skull"),
+    EmojiItem("\uD83C\uDF82", "Birthday Cake"),
+    EmojiItem("\uD83C\uDF39", "Rose"),
+    EmojiItem("\uD83C\uDF1F", "Sun with Face"),
+    EmojiItem("\uD83C\uDF08", "Rainbow"),
+    EmojiItem("\u2600\uFE0F", "Black Sun with Rays"),
+    EmojiItem("\uD83D\uDC36", "Dog Face"),
+    EmojiItem("\uD83D\uDC31", "Cat Face"),
+    EmojiItem("\uD83D\uDC29", "Poodle"),
+    EmojiItem("\uD83D\uDC14", "Chicken"),
+    EmojiItem("\uD83E\uDD8B", "Butterfly")
+)
--- app/src/main/java/com/decathlon/assitedinjectsample/MainActivity.kt
+++ app/src/main/java/com/decathlon/assitedinjectsample/MainActivity.kt
@@ -4,24 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.decathlon.assitedinjectsample.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.serialization.Serializable
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,17 +38,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
 
+
                     val navController = rememberNavController()
 
                     NavHost(
                         modifier = Modifier.padding(innerPadding),
                         navController = navController,
-                        startDestination = GameRoute(level = 10)
+                        startDestination = ListRoute
                     ) {
-                        composable<GameRoute> {
-                            GameScreen(
-                                title = "Game",
+
+                        composable<ListRoute> {
+                            ListScreen(
+                                navigateToDetailRoute = {
+                                    navController.navigate(DetailRoute(it))
+                                }
                             )
+                        }
+
+                        composable<DetailRoute> { navBackStackEntry ->
+                            val index: Int = navBackStackEntry.toRoute<DetailRoute>().index
+                            DetailScreen(index)
                         }
 
                     }
@@ -50,36 +66,60 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    private fun DetailScreen(
+        index: Int,
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = emojiItems[index].emoji,
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    }
+
+    @Composable
+    private fun ListScreen(
+        modifier: Modifier = Modifier,
+        navigateToDetailRoute: (Int) -> Unit,
+    ) {
+        LazyColumn(
+            modifier = modifier,
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            itemsIndexed(
+                key = { index, _ -> index },
+                items = emojiItems
+            ) { index, item ->
+                ListItem(
+                    modifier = Modifier.clickable {
+                        navigateToDetailRoute(index)
+                    },
+                    headlineContent = {
+                        Text(
+                            text = item.description,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    },
+                    leadingContent = {
+                        Text(
+                            text = item.emoji,
+                            style = MaterialTheme.typography.displayMedium
+                        )
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Serializable
-data class GameRoute(val level: Int)
+object ListRoute
 
-
-@Composable
-fun GameScreen(
-    title: String,
-    viewModel: GameViewModel = hiltViewModel<GameViewModel>(),
-    modifier: Modifier = Modifier,
-) {
-    Column {
-        Text(
-            text = title,
-            modifier = modifier,
-        )
-        Text(
-            text = viewModel.level.toString(),
-            modifier = modifier,
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        GameScreen(
-            title = "Android"
-        )
-    }
-}
+@Serializable
+data class DetailRoute(val index: Int)
